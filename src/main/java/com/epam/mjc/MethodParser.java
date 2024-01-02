d39d1bb5d2e6a6560c49cc545387a19db4cac6cc
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-01-02 23:50:39 +0400

    solution for MethodParser added

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,10 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MethodParser {
 
     /**
@@ -20,6 +25,41 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        var signatures = Arrays.stream(signatureString.split(" ")).collect(Collectors.toList());
+        String accessModifier = null;
+        if (signatures.contains("public") || signatures.contains("private") || signatures.contains("protected")) {
+            accessModifier = signatures.get(0);
+            signatures.remove(0);
+        }
+        var returnType = signatures.get(0);
+        var methodName = signatures.get(1).split("\\(")[0];
+        var arguments = new ArrayList<MethodSignature.Argument>();
+        if (!signatures.get(1).contains(")")) {
+            getArguments(arguments, signatures);
+        }
+        var methodSignature = new MethodSignature(methodName, arguments);
+        methodSignature.setAccessModifier(accessModifier);
+        methodSignature.setReturnType(returnType);
+        return methodSignature;
+    }
+
+    private void getArguments(List<MethodSignature.Argument> arguments, List<String> signatures) {
+        arguments.add(new MethodSignature.Argument(signatures.get(1).split("\\(")[1],
+                signatures.get(2).replace(",", "")));
+        String parameterType = null;
+        String parameterName = null;
+        for (var i = 3; i < signatures.size(); i++) {
+            if (signatures.get(i).contains(",")) {
+                parameterName = signatures.get(i).replace(",", "");
+            } else if (signatures.get(i).contains(")")) {
+                parameterName = signatures.get(i).replace(")", "");
+            } else {
+                parameterType = signatures.get(i);
+            }
+            if (parameterName != null) {
+                arguments.add(new MethodSignature.Argument(parameterType, parameterName));
+                parameterName = null;
+            }
+        }
     }
 }
